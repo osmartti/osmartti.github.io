3751fa1a25a8b95f5b7234c0defad7fe3c60d985
Ossi Marttinen <ossi.marttinen@neomore.fi>
2026-03-08 14:14:26 +0200

    add godot to technologies

--- src/components/WorkProjectsSection.svelte
+++ src/components/WorkProjectsSection.svelte
@@ -1,211 +1,215 @@
 <div id="work">
-    <div class="topic-container fade-in delay-1" style="display: block;">
-        <div>
-            <h2>Projects & Roles</h2>
-            <div class="work-projects-container fade-in-text delay-2">
-                <div class="work-project">
-                    <h3>Employee Experience</h3>
-                    <h4>2025</h4>
-                    <p>
-                        Worked as lead developer (<b>React + Azure Functions</b
-                        >), architect (<b>Azure</b>) & Scrum Master. Created an
-                        inhouse tool for upkeeping talents skills,
-                        certifications and CVs. Included also data analytics for
-                        managers to monitor their employees career growth &
-                        experience.
-                    </p>
-                </div>
-                <div class="work-project">
-                    <h3>Material Master Data Suite - product</h3>
-                    <h4>2024</h4>
-                    <p>
-                        Worked as lead developer (<b>SAPUI5 + SAP CAP</b>),
-                        architect (<b>SAP BTP</b>) & Scrum Master. Created an
-                        inhouse product for creating material with more ease to
-                        upkeep master data.
-                    </p>
-                </div>
-                <div class="work-project">
-                    <h3>Steelfinder</h3>
-                    <h4>2023</h4>
-                    <p>
-                        Worked as Azure architect + API developer (<b
-                            >NodeJS + PSQL</b
-                        >) + Mendix developer. Client wanted a product where
-                        anyone interested in buying steel can find their
-                        offerings, check their chemical compositions & corrosion
-                        data.
-                    </p>
-                </div>
-                <div class="work-project">
-                    <h3>RCA - product</h3>
-                    <h4>2023</h4>
-                    <p>
-                        Worked as full-stack developer (<b
-                            >React + AWS Lamda + MySQL</b
-                        >) and planned AWS architecture. Created a product for
-                        client. Main focus on this project was to make it
-                        dynamical to fit client's customers needs. Docker was
-                        used for AWS Lambdas + Databases in local development.
-                    </p>
-                </div>
-                <div class="work-project">
-                    <h3>RCA tool</h3>
-                    <h4>2021</h4>
-                    <p>
-                        Worked as full-stack developer (<b
-                            >React + Java + MySQL</b
-                        >) for Root Cause Analysis tool. Tool is heavily used by
-                        the customer daily, in multiple countries.
-                    </p>
-                </div>
-                <div class="work-project">
-                    <h3>SAP Cloud Foundry environment set-up</h3>
-                    <h4>2020</h4>
-                    <p>
-                        Created SAP BTP Cloud Foundry environments for
-                        development and production use. Configured all necessary
-                        services and migrated first applications from NEO cloud.
-                    </p>
-                </div>
-                <div class="work-project">
-                    <h3>Loading cockpit</h3>
-                    <h4>2020</h4>
-                    <p>
-                        Worked as front-end developer (<b>SAPUI5</b>). Created a
-                        application that was used by warehouse workers to manage
-                        their outbound data. Made integrations to 3rd party API,
-                        which was used for RFID scanning.
-                    </p>
-                </div>
-                <div class="work-project">
-                    <h3>Assembly planner tool</h3>
-                    <h4>2019</h4>
-                    <p>
-                        Worked as front-end developer (<b>SAPUI5</b>). Created a
-                        application that was used for planning shipbuilding.
-                    </p>
-                </div>
-                <div class="work-project">
-                    <h3>PIRA&trade; -service</h3>
-                    <h4>2018</h4>
-                    <p>
-                        Worked as full-stack developer (<b
-                            >Vue + NodeJS/Java + PSQL</b
-                        >). Developed application was used to create risk
-                        assesments for SME companies in Finland by Finnish
-                        Institute of Occupational Health
-                    </p>
-                </div>
-            </div>
-        </div>
-    </div>
-    <div class="topic-container fade-in delay-1" style="display: block;">
-        <div>
-            <h2>Technologies & Tools</h2>
-            <div class="technologies-container fade-in-text delay-2">
-                <div class="technology-container">
-                    <img
-                        src="images/technologies/javascript.png"
-                        alt="javascript.png"
-                    />
-                    <p>JavaScript</p>
-                </div>
-                <div class="technology-container">
-                    <img src="images/technologies/html5.png" alt="html5.png" />
-                    <p>HTML5</p>
-                </div>
-                <div class="technology-container">
-                    <img src="images/technologies/css.png" alt="css.png" />
-                    <p>CSS</p>
-                </div>
-                <div class="technology-container">
-                    <img
-                        src="images/technologies/typescript.png"
-                        alt="typescript.png"
-                    />
-                    <p>TypeScript</p>
-                </div>
-                <div class="technology-container">
-                    <img
-                        src="images/technologies/nodejs.png"
-                        alt="nodejs.png"
-                    />
-                    <p>NodeJS</p>
-                </div>
-                <div class="technology-container">
-                    <img src="images/technologies/java.png" alt="java.png" />
-                    <p>Java</p>
-                </div>
-                <div class="technology-container">
-                    <img src="images/technologies/ui5.png" alt="ui5.png" />
-                    <p>SAP UI5</p>
-                </div>
-                <div class="technology-container">
-                    <img src="images/technologies/cap.png" alt="cap.png" />
-                    <p>SAP CAP</p>
-                </div>
-                <div class="technology-container">
-                    <img src="images/technologies/mysql.png" alt="mysql.png" />
-                    <p>MySQL</p>
-                </div>
-                <div class="technology-container">
-                    <img
-                        src="images/technologies/postgresql.png"
-                        alt="postgresql.png"
-                    />
-                    <p>PSQL</p>
-                </div>
-                <div class="technology-container">
-                    <img src="images/technologies/react.png" alt="react.png" />
-                    <p>React</p>
-                </div>
-                <div class="technology-container">
-                    <img
-                        src="images/technologies/docker.png"
-                        alt="docker.png"
-                    />
-                    <p>Docker</p>
-                </div>
-                <div class="technology-container">
-                    <img
-                        src="images/technologies/python.png"
-                        alt="python.png"
-                    />
-                    <p>Python</p>
-                </div>
-                <div class="technology-container">
-                    <img src="images/technologies/bash.png" alt="bash.png" />
-                    <p>Bash</p>
-                </div>
-                <div class="technology-container">
-                    <img src="images/technologies/git.png" alt="git.png" />
-                    <p>Git</p>
-                </div>
-                <div class="technology-container">
-                    <img src="images/technologies/btp.png" alt="btp.png" />
-                    <p>SAP BTP</p>
-                </div>
-                <div class="technology-container">
-                    <img src="images/technologies/aws.png" alt="aws.png" />
-                    <p>AWS</p>
-                </div>
-                <div class="technology-container">
-                    <img src="images/technologies/azure.png" alt="azure.png" />
-                    <p>Azure</p>
-                </div>
-                <div class="technology-container">
-                    <img
-                        src="images/technologies/svelte.png"
-                        alt="svelte.png"
-                    />
-                    <p>Svelte</p>
-                </div>
-                <div class="technology-container">
-                    <img src="images/technologies/vue.png" alt="vue.png" />
-                    <p>Vue</p>
-                </div>
-            </div>
-        </div>
-    </div>
+	<div class="topic-container fade-in delay-1" style="display: block;">
+		<div>
+			<h2>Projects & Roles</h2>
+			<div class="work-projects-container fade-in-text delay-2">
+				<div class="work-project">
+					<h3>Employee Experience</h3>
+					<h4>2025</h4>
+					<p>
+						Worked as lead developer (<b>React + Azure Functions</b
+						>), architect (<b>Azure</b>) & Scrum Master. Created an
+						inhouse tool for upkeeping talents skills,
+						certifications and CVs. Included also data analytics for
+						managers to monitor their employees career growth &
+						experience.
+					</p>
+				</div>
+				<div class="work-project">
+					<h3>Material Master Data Suite - product</h3>
+					<h4>2024</h4>
+					<p>
+						Worked as lead developer (<b>SAPUI5 + SAP CAP</b>),
+						architect (<b>SAP BTP</b>) & Scrum Master. Created an
+						inhouse product for creating material with more ease to
+						upkeep master data.
+					</p>
+				</div>
+				<div class="work-project">
+					<h3>Steelfinder</h3>
+					<h4>2023</h4>
+					<p>
+						Worked as Azure architect + API developer (<b
+							>NodeJS + PSQL</b
+						>) + Mendix developer. Client wanted a product where
+						anyone interested in buying steel can find their
+						offerings, check their chemical compositions & corrosion
+						data.
+					</p>
+				</div>
+				<div class="work-project">
+					<h3>RCA - product</h3>
+					<h4>2023</h4>
+					<p>
+						Worked as full-stack developer (<b
+							>React + AWS Lamda + MySQL</b
+						>) and planned AWS architecture. Created a product for
+						client. Main focus on this project was to make it
+						dynamical to fit client's customers needs. Docker was
+						used for AWS Lambdas + Databases in local development.
+					</p>
+				</div>
+				<div class="work-project">
+					<h3>RCA tool</h3>
+					<h4>2021</h4>
+					<p>
+						Worked as full-stack developer (<b
+							>React + Java + MySQL</b
+						>) for Root Cause Analysis tool. Tool is heavily used by
+						the customer daily, in multiple countries.
+					</p>
+				</div>
+				<div class="work-project">
+					<h3>SAP Cloud Foundry environment set-up</h3>
+					<h4>2020</h4>
+					<p>
+						Created SAP BTP Cloud Foundry environments for
+						development and production use. Configured all necessary
+						services and migrated first applications from NEO cloud.
+					</p>
+				</div>
+				<div class="work-project">
+					<h3>Loading cockpit</h3>
+					<h4>2020</h4>
+					<p>
+						Worked as front-end developer (<b>SAPUI5</b>). Created a
+						application that was used by warehouse workers to manage
+						their outbound data. Made integrations to 3rd party API,
+						which was used for RFID scanning.
+					</p>
+				</div>
+				<div class="work-project">
+					<h3>Assembly planner tool</h3>
+					<h4>2019</h4>
+					<p>
+						Worked as front-end developer (<b>SAPUI5</b>). Created a
+						application that was used for planning shipbuilding.
+					</p>
+				</div>
+				<div class="work-project">
+					<h3>PIRA&trade; -service</h3>
+					<h4>2018</h4>
+					<p>
+						Worked as full-stack developer (<b
+							>Vue + NodeJS/Java + PSQL</b
+						>). Developed application was used to create risk
+						assesments for SME companies in Finland by Finnish
+						Institute of Occupational Health
+					</p>
+				</div>
+			</div>
+		</div>
+	</div>
+	<div class="topic-container fade-in delay-1" style="display: block;">
+		<div>
+			<h2>Technologies & Tools</h2>
+			<div class="technologies-container fade-in-text delay-2">
+				<div class="technology-container">
+					<img
+						src="images/technologies/javascript.png"
+						alt="javascript.png"
+					/>
+					<p>JavaScript</p>
+				</div>
+				<div class="technology-container">
+					<img src="images/technologies/html5.png" alt="html5.png" />
+					<p>HTML5</p>
+				</div>
+				<div class="technology-container">
+					<img src="images/technologies/css.png" alt="css.png" />
+					<p>CSS</p>
+				</div>
+				<div class="technology-container">
+					<img
+						src="images/technologies/typescript.png"
+						alt="typescript.png"
+					/>
+					<p>TypeScript</p>
+				</div>
+				<div class="technology-container">
+					<img
+						src="images/technologies/nodejs.png"
+						alt="nodejs.png"
+					/>
+					<p>NodeJS</p>
+				</div>
+				<div class="technology-container">
+					<img src="images/technologies/java.png" alt="java.png" />
+					<p>Java</p>
+				</div>
+				<div class="technology-container">
+					<img src="images/technologies/ui5.png" alt="ui5.png" />
+					<p>SAP UI5</p>
+				</div>
+				<div class="technology-container">
+					<img src="images/technologies/cap.png" alt="cap.png" />
+					<p>SAP CAP</p>
+				</div>
+				<div class="technology-container">
+					<img src="images/technologies/mysql.png" alt="mysql.png" />
+					<p>MySQL</p>
+				</div>
+				<div class="technology-container">
+					<img
+						src="images/technologies/postgresql.png"
+						alt="postgresql.png"
+					/>
+					<p>PSQL</p>
+				</div>
+				<div class="technology-container">
+					<img src="images/technologies/react.png" alt="react.png" />
+					<p>React</p>
+				</div>
+				<div class="technology-container">
+					<img
+						src="images/technologies/docker.png"
+						alt="docker.png"
+					/>
+					<p>Docker</p>
+				</div>
+				<div class="technology-container">
+					<img
+						src="images/technologies/python.png"
+						alt="python.png"
+					/>
+					<p>Python</p>
+				</div>
+				<div class="technology-container">
+					<img src="images/technologies/bash.png" alt="bash.png" />
+					<p>Bash</p>
+				</div>
+				<div class="technology-container">
+					<img src="images/technologies/git.png" alt="git.png" />
+					<p>Git</p>
+				</div>
+				<div class="technology-container">
+					<img src="images/technologies/btp.png" alt="btp.png" />
+					<p>SAP BTP</p>
+				</div>
+				<div class="technology-container">
+					<img src="images/technologies/aws.png" alt="aws.png" />
+					<p>AWS</p>
+				</div>
+				<div class="technology-container">
+					<img src="images/technologies/azure.png" alt="azure.png" />
+					<p>Azure</p>
+				</div>
+				<div class="technology-container">
+					<img
+						src="images/technologies/svelte.png"
+						alt="svelte.png"
+					/>
+					<p>Svelte</p>
+				</div>
+				<div class="technology-container">
+					<img src="images/technologies/vue.png" alt="vue.png" />
+					<p>Vue</p>
+				</div>
+				<div class="technology-container">
+					<img src="images/technologies/godot.png" alt="godot.png" />
+					<p>Godot</p>
+				</div>
+			</div>
+		</div>
+	</div>
 </div>
